9fff735cb14033c1c09456a3709c7b1415f207eb
Basu0807 <vasupandeyji.98@gmail.com>
2023-11-20 13:26:44 +0530

    link removed

--- src/components/Routes/AddToCart.js
+++ src/components/Routes/AddToCart.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import{AddItem,SubItem} from '../Redux/ItemCountSlice'
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const AddToCart = () => {
   const CartItems=useSelector((state)=>state.InDe)
@@ -13,25 +13,33 @@ const AddToCart = () => {
       <div className='Cart_Container'>
 
         <table className='CartTable'>
+          <thead>
           <tr>
             <td>Item Image</td>
             <td>Model</td>
             <td>Quantity</td>
             <td>Price</td>
           </tr>
+          </thead>
+          
           
             
             {
     CartItems && CartItems.map((item,index)=>{
       return(
-       <>
-       <tr>
+      <tbody key={index}>
+
+<tr >
         <td><img src={item.image} alt='product_image'/></td>
        <td>{item.Model}</td>
        <td><button onClick={()=>dispatch(AddItem(1))}>+</button>{itemCount}<button onClick={()=>dispatch(SubItem(1))}>-</button></td>
        <td>{item.price}</td>
        </tr>
-       </> 
+      </tbody>
+       
+
+       
+       
        
         
       )
@@ -45,14 +53,6 @@ const AddToCart = () => {
      
   )
 }
-{/* <div className='Cart_Product' key={index}>
-<Link to={"/Data/"+item._id} state={{id:item._id}}>
-<div className='image_section'><img src={item.image} alt='product_image'/></div>
-</Link>
-<div>
-  <p>{item.heading}</p>
-  <p>{item.price}</p>
-</div>
-</div> */}
+
 
 export default AddToCart
